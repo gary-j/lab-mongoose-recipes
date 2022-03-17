351f4d74f4e89e26a368b4535093452abb031044
Dev deRev <devderev@free.fr>
2022-03-17 15:09:17 +0100

    iteration 2 done

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ mongoose
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    // return Recipe.deleteMany()
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
@@ -21,3 +21,32 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  // Iteration 2 - Create a recipe
+  const recipeDetails = {
+      "title": "Asian Glazed Chicken Thighs",
+      "level": "Amateur Chef",
+      "ingredients": [
+        "1/2 cup rice vinegar",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste",
+        "8 skinless, boneless chicken thighs"
+      ],
+      "cuisine": "Asian",
+      "dishType": "main_course",
+      "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      "duration": 40,
+      "creator": "Chef LePapu"
+    }
+  
+  Recipe.create(recipeDetails)
+  .then(
+    (recipe)=> console.log('Received successfully saved: ', recipe)
+  )
+  .catch(
+    (error)=> console.error('an error occured while saving the recipes, try again: ', error)
+  )
